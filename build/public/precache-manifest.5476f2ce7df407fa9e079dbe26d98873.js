fe708e190a8ceee9ec9b5f273bb963166c60f14d
Riley-March <riley.j.march@outlook.com>
2021-02-24 15:45:26 +1000

    fixed public folder

---
+++ build/public/precache-manifest.5476f2ce7df407fa9e079dbe26d98873.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63cdc8d5755e40776f9436237d858bab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "901acdea9545a5ad7f18",
+    "url": "/static/css/2.13e694c3.chunk.css"
+  },
+  {
+    "revision": "9cad51fefbfaca147adc",
+    "url": "/static/css/main.973237c2.chunk.css"
+  },
+  {
+    "revision": "901acdea9545a5ad7f18",
+    "url": "/static/js/2.2e84d0f1.chunk.js"
+  },
+  {
+    "revision": "9858eb7295a834f79bacaa7a363c10a4",
+    "url": "/static/js/2.2e84d0f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cad51fefbfaca147adc",
+    "url": "/static/js/main.53459b05.chunk.js"
+  },
+  {
+    "revision": "63418c513088d6d143db",
+    "url": "/static/js/runtime-main.7c3fa1f1.js"
+  }
+]);
